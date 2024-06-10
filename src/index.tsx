9b6a73fd4017bdad8fdca7a19943b5f6da8e2984
Marco Corapi <marco.corapi@outlook.it>
2024-06-10 14:30:18 +0200

    Fixed service worker configuration and added gh-pages deployment

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,21 @@ import ReactDOM, { Container } from "react-dom/client";
 
 import store from "./redux/store";
 import { App } from "./App";
+import { worker } from "./mocks/mocks";
+
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
+  const serviceWorkerUrl = process.env.NODE_ENV === 'development' ? '/mockServiceWorker.js' : `${process.env.PUBLIC_URL}/mockServiceWorker.js`;
+  console.log('Service Worker URL:', serviceWorkerUrl);
 
-  const { worker } = await import("./mocks/mocks");
-
-  return worker.start();
+  return worker.start({
+    serviceWorker: {
+      url: serviceWorkerUrl,
+    },
+    onUnhandledRequest: 'bypass',
+  });
 }
+
 
 const rootDiv = document.getElementById("root") as Container;
   const root = ReactDOM.createRoot(rootDiv);
