e76ff0fe27020b5767d1a56cc309f2a5d1646b93
Marco Corapi <marco.corapi@outlook.it>
2024-06-10 18:34:01 +0200

    update config

--- config-overrides.js
+++ config-overrides.js
@@ -3,24 +3,24 @@ const CopyPlugin = require("copy-webpack-plugin");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = function override(config, env) {
-  // Remove the ModuleScopePlugin
   config.resolve.plugins = config.resolve.plugins.filter(
     (plugin) => !(plugin instanceof ModuleScopePlugin)
   );
 
-  // Add CopyPlugin
   config.plugins.push(
     new CopyPlugin({
       patterns: [
         {
           from: "./public/mockServiceWorker.js",
-          to: env === "production" ? "./code-challenge-test" : "./",
+          to:
+            env === "production"
+              ? "code-challenge-test/mockServiceWorker.js"
+              : "./mockServiceWorker.js",
         },
       ],
     })
   );
 
-  // Add React Refresh plugin for development environment
   if (env === "development") {
     config.plugins.push(new ReactRefreshWebpackPlugin());
 
